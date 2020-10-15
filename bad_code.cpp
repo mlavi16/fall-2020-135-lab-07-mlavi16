88f2c99fe237959a125074225f0b40516f281673
mlavi16 <Maya.lavi11@gmail.com>
2020-10-14 21:11:02 -0400

    initial commit

---
+++ bad_code.cpp
@@ -0,0 +1,11 @@
+        #include <iostream>
+      using namespace std;
+
+                int main(){
+           // Hi, I'm a program!
+int x = 1; 
+    for(int i = 0; i < 10; i++) {
+cout << i;
+          cout << endl;
+ }
+    }
